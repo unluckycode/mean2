7edaa16ad472bfa9d4c5068a2f68961047c9277f
piotrlukasik <piotrlukasik@hotmail.com>
2017-03-22 01:45:27 +0100

    Adaptation project to use Angular2.

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styles: [`
+        .author{
+        display: inline-block;
+        font-style: italic;
+        font-size: 12px;
+        width: 80%;
+        }
+        .config{
+        display: inline-block;
+        text-align: right;
+        font-size: 12px;
+        width: 19%;
+        }
+    `]
 })
 export class AppComponent {
-    
+    message = {
+        content: 'A massage',
+        author: 'Piotr'
+    };
 }
--- routes/app.js
+++ routes/app.js
@@ -1,32 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user')
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function (err, doc) {
-        if (err) {
-            return res.send('Error!');
-        }
-
-        res.render('nodde',{email:doc.email});
-    });
+    res.render('index');
 });
 
-/*
- router.get('/message/:msg',function (req,res,next) {
- res.render('nodde', {message: req.params.msg});
- })
- */
-
-router.post('/', function (req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Piotr',
-        lastName: 'Lukasik',
-        password: 'secret',
-        email: email
-    });
-    user.save();
-    res.redirect('/');
-})
 module.exports = router;
